e135939116f7aecd762613a53a1adb0d33b1398d
ceilf6 <wangjinghong02@meituan.com>
2026-04-29 20:33:56 +0800

    fix: dist/index.mjs 是 ESM bundle，但打包进去的 TypeScript/Node 代码还在访问 CommonJS 全局变量 __filename，所以启动时直接炸了

--- build.mjs
+++ build.mjs
@@ -33,7 +33,11 @@ await build({
     js: [
       '#!/usr/bin/env node',
       'import { createRequire as __createRequire } from "module";',
+      'import { fileURLToPath as __fileURLToPath } from "url";',
+      'import { dirname as __pathDirname } from "path";',
       'const require = __createRequire(import.meta.url);',
+      'const __filename = __fileURLToPath(import.meta.url);',
+      'const __dirname = __pathDirname(__filename);',
     ].join('\n'),
   },
   minify: false,
